308ae52992a583b7440bd10118180a89efbe3a02
hotequil <hotequil@protonmail.com>
2022-09-22 19:32:24 -0300

    feat: added new attributes in Category class

--- src/entities/category.py
+++ src/entities/category.py
@@ -1,11 +1,8 @@
 class Category:
-    def __init__(self, name):
-        self._name = name
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, name):
-        self._name = name
+    def __init__(self, category_name, dt_created, created_by_user_id = None, category_id = None, is_active = True, dt_updated = None):
+        self._category_id = category_id
+        self._category_name = category_name
+        self._created_by_user_id = created_by_user_id
+        self._is_active = is_active
+        self._dt_created = dt_created
+        self._dt_updated = dt_updated
