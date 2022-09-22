bb1f60730546f6f3875c67bb9427fae1b5ea9da2
hotequil <hotequil@protonmail.com>
2022-09-22 19:35:33 -0300

    feat: added Situation enum

--- src/entities/category.py
+++ src/entities/category.py
@@ -1,5 +1,7 @@
+from src.enums.situation import Situation
+
 class Category:
-    def __init__(self, category_name, dt_created, created_by_user_id = None, category_id = None, is_active = True, dt_updated = None):
+    def __init__(self, category_name, dt_created, created_by_user_id = None, category_id = None, is_active = Situation.ACTIVE, dt_updated = None):
         self._category_id = category_id
         self._category_name = category_name
         self._created_by_user_id = created_by_user_id
