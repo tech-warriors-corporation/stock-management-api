48622d22b850d1800e951db66030546f646da53f
hotequil <hotequil@protonmail.com>
2022-09-22 20:59:00 -0300

    test: created instance of Category

--- src/test.py
+++ src/test.py
@@ -1,7 +1,12 @@
 from src.enums.boolean_as_number import BooleanAsNumber
 from src.entities.user import User
+from src.entities.category import Category
 from datetime import datetime
 
-user = User(None, 'admin', 'email@email.email', 'password', BooleanAsNumber.TRUE.value, BooleanAsNumber.TRUE.value, datetime.now(), None, None)
+registered_id = 1
+date = datetime.now()
+user = User(registered_id, 'admin', 'email@email.email', 'password', BooleanAsNumber.TRUE.value, BooleanAsNumber.TRUE.value, date, None, None)
+category = Category(registered_id, 'school', registered_id, BooleanAsNumber.TRUE.value, date, None)
 
 print(user)
+print(category)
