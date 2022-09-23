03ab94e87c7c5be028fe824e6922016388026210
hotequil <hotequil@protonmail.com>
2022-09-22 21:04:21 -0300

    test: created instance of Output

--- src/test.py
+++ src/test.py
@@ -2,6 +2,7 @@ from src.enums.boolean_as_number import BooleanAsNumber
 from src.entities.user import User
 from src.entities.category import Category
 from src.entities.product import Product
+from src.entities.output import Output
 from datetime import datetime
 
 registered_id = 1
@@ -9,7 +10,9 @@ date = datetime.now()
 user = User(registered_id, 'admin', 'email', 'password', BooleanAsNumber.TRUE.value, BooleanAsNumber.TRUE.value, date, None, None)
 category = Category(registered_id, 'school', registered_id, BooleanAsNumber.TRUE.value, date, None)
 product = Product(registered_id, registered_id, 'apple', 5, registered_id, BooleanAsNumber.TRUE.value, date, None)
+output = Output(registered_id, registered_id, 3, BooleanAsNumber.FALSE.value, 'person name', registered_id, BooleanAsNumber.TRUE.value, date, date, None, 'product output')
 
 print(user)
 print(category)
 print(product)
+print(output)
