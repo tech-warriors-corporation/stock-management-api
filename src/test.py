d3b4e006f47422e22b30be6a13c4c22104f4d116
hotequil <hotequil@protonmail.com>
2022-09-22 21:09:23 -0300

    test: created instance of Input

--- src/test.py
+++ src/test.py
@@ -3,6 +3,7 @@ from src.entities.user import User
 from src.entities.category import Category
 from src.entities.product import Product
 from src.entities.output import Output
+from src.entities.input import Input
 from datetime import datetime
 
 registered_id = 1
@@ -11,8 +12,10 @@ user = User(registered_id, 'admin', 'email', 'password', BooleanAsNumber.TRUE.va
 category = Category(registered_id, 'school', registered_id, BooleanAsNumber.TRUE.value, date, None)
 product = Product(registered_id, registered_id, 'apple', 5, registered_id, BooleanAsNumber.TRUE.value, date, None)
 output = Output(registered_id, registered_id, 3, BooleanAsNumber.FALSE.value, 'person name', registered_id, BooleanAsNumber.TRUE.value, date, date, None, 'product output')
+product_input = Input(registered_id, registered_id, 4, BooleanAsNumber.FALSE.value, BooleanAsNumber.FALSE.value, registered_id, BooleanAsNumber.TRUE.value, date, date, None, 400, 'product input')
 
 print(user)
 print(category)
 print(product)
 print(output)
+print(product_input)
