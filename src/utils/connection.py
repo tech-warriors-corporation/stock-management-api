f294aa73151b158c8220f2cd9a970f04370be6b1
hotequil <hotequil@protonmail.com>
2022-09-22 22:46:36 -0300

    feat: created connection to db file

---
+++ src/utils/connection.py
@@ -0,0 +1,17 @@
+import oracledb
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv())
+
+_connection = None
+
+def get_connection():
+    global _connection
+
+    if _connection is not None:
+        return _connection.cursor()
+
+    _connection = oracledb.connect(user=os.environ.get('DB_USER'), password=os.environ.get('DB_PASSWORD'), host=os.environ.get('DB_HOST'), port=os.environ.get('DB_PORT'), sid=os.environ.get('DB_SID'))
+
+    return get_connection()
