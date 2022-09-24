3fdb2ce27388c077597fe2c74bc8e707d2e70279
hotequil <hotequil@protonmail.com>
2022-09-24 11:34:31 -0300

    feat: created EnvVar enum

---
+++ src/enums/env_var.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class EnvVar(Enum):
+    DB_USER = 'DB_USER'
+    DB_PASSWORD = 'DB_PASSWORD'
+    DB_HOST = 'DB_HOST'
+    DB_PORT = 'DB_PORT'
+    DB_SID = 'DB_SID'
+    DEBUG_MODE = 'DEBUG_MODE'
