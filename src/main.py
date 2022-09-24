7eae45bc2ad83f26eaafb36f00cd5694d108a1b4
hotequil <hotequil@protonmail.com>
2022-09-24 11:35:23 -0300

    feat: putted loadenv in main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
 from src.services.app import app
+from dotenv import load_dotenv, find_dotenv
+from os import environ
+from src.enums.env_var import EnvVar
 
-app.run()
+load_dotenv(find_dotenv())
+
+import src.routes.login
+
+app.run(debug=eval(environ.get(EnvVar.DEBUG_MODE.value)))
