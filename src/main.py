ad60c08d63e146463f7f762ac091a92d560e6843
hotequil <hotequil@protonmail.com>
2022-09-25 21:19:02 -0300

    feat: created ORIGIN variable

--- src/main.py
+++ src/main.py
@@ -1,10 +1,6 @@
 from src.services.setup import app
-from dotenv import load_dotenv, find_dotenv
 from os import environ
 from src.enums.env_var import EnvVar
-
-load_dotenv(find_dotenv())
-
 import src.routes.auth
 
 app.run(debug=eval(environ.get(EnvVar.DEBUG_MODE.value)))
--- src/services/setup.py
+++ src/services/setup.py
@@ -1,7 +1,12 @@
+from dotenv import load_dotenv, find_dotenv
 from flask import Flask
 from flask_cors import CORS
 from src.utils.constants import api_prefix
+from os import environ
+from src.enums.env_var import EnvVar
+
+load_dotenv(find_dotenv())
 
 app = Flask(__name__)
 
-CORS(app, resources={rf"/{api_prefix}/*": { "origins": ["https://sistema-de-estoque-projeto-casulo.netlify.app", "http://localhost:4200"] }})
+CORS(app, resources={rf"/{api_prefix}/*": { "origins": [environ.get(EnvVar.ORIGIN.value)] }})
