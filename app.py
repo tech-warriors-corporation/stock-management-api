89c2b82c2e4f2daf435caea2b075e1994d18960f
hotequil <hotequil@protonmail.com>
2022-09-26 23:37:24 -0300

    feat: test CORS

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ load_dotenv(find_dotenv())
 app = Flask(__name__)
 
 CORS(app, resources={r"/*": { "origins": "*" }}, supports_credentials=True)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.after_request
 def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   response.headers.add('Access-Control-Allow-Credentials', 'true')
