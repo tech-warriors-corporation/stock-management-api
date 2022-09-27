c8c3385c8be696c7343fcac944a7f56cd2d6d46a
hotequil <hotequil@protonmail.com>
2022-09-27 20:20:26 -0300

    feat: adjusted JWT

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 The API for consult data from Casulo Social (NGO).
 
 ## Setup
-Create an `.env` file with `DB_USER`, `DB_PASSWORD`, `DB_HOST`, `DB_PORT`, `DB_SID`, `DEBUG_MODE`, `JWT_SECRET`, `JWT_ALGORITHM`, `JWT_CHARSET` and `ORIGIN` variables to work.
+Create an `.env` file with `DB_USER`, `DB_PASSWORD`, `DB_HOST`, `DB_PORT`, `DB_SID`, `DEBUG_MODE`, `JWT_SECRET`, `JWT_ALGORITHM`, `ORIGIN` and `PORT` variables to work.
 
 ## Start
 Run `python -m app`.
--- utils/token.py
+++ utils/token.py
@@ -4,10 +4,9 @@ from jwt import encode, decode
 
 _secret = environ.get(EnvVar.JWT_SECRET.value)
 _algorithm = environ.get(EnvVar.JWT_ALGORITHM.value)
-_charset = environ.get(EnvVar.JWT_CHARSET.value)
 
 def encoder(data):
-    return encode(data, _secret, algorithm=_algorithm).decode(_charset)
+    return encode(data, _secret, algorithm=_algorithm)
 
 def decoder(token):
-    return decode(token, _secret, False, [_algorithm])
+    return decode(token, _secret, [_algorithm])
