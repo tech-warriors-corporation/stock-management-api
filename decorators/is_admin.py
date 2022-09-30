9c5300ea5c75a0016a072930dd4573dc95824548
hotequil <hotequil@protonmail.com>
2022-09-30 19:13:49 -0300

    feat: updated secure_function to not repeat

--- decorators/is_admin.py
+++ decorators/is_admin.py
@@ -5,13 +5,15 @@ from utils.token import decoder
 from enums.boolean_as_number import BooleanAsNumber
 
 def is_admin(callback):
-    def secure_function():
+    def secure_function(*args, **kwargs):
         token = request.headers.get(HeaderRequest.TOKEN.value)
         user = decoder(token)
 
         if user['is_admin'] == BooleanAsNumber.TRUE.value:
-            return callback()
+            return callback(*args, **kwargs)
         else:
             return unauthorized_response()
+
+    secure_function.__name__ = callback.__name__
 
     return secure_function
--- decorators/login_required.py
+++ decorators/login_required.py
@@ -3,12 +3,14 @@ from utils.auth import has_valid_token, unauthorized_response
 from enums.header_request import HeaderRequest
 
 def login_required(callback):
-    def secure_function():
+    def secure_function(*args, **kwargs):
         token = request.headers.get(HeaderRequest.TOKEN.value)
 
         if has_valid_token(token):
-            return callback()
+            return callback(*args, **kwargs)
         else:
             return unauthorized_response()
+
+    secure_function.__name__ = callback.__name__
 
     return secure_function
