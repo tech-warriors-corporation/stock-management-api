060d120fd7e5aa73aacf38737121ec5132edc2aa
hotequil <hotequil@protonmail.com>
2022-10-10 19:54:33 -0300

    feat: created table name for product and added a new attribute for entity

--- entities/product.py
+++ entities/product.py
@@ -1,5 +1,5 @@
 class Product:
-    def __init__(self, product_id, category_id, product_name, quantity, created_by_user_id, is_active, dt_created, dt_updated):
+    def __init__(self, product_id, category_id, product_name, quantity, created_by_user_id, is_active, dt_created, dt_updated, category_name):
         self.product_id = product_id
         self.category_id = category_id
         self.product_name = product_name
@@ -8,3 +8,4 @@ class Product:
         self.is_active = is_active
         self.dt_created = dt_created
         self.dt_updated = dt_updated
+        self.category_name = category_name
--- enums/table.py
+++ enums/table.py
@@ -3,3 +3,4 @@ from enum import Enum
 class Table(Enum):
     USERS = 'T_STM_USER'
     CATEGORIES = 'T_STM_CATEGORY'
+    PRODUCTS = 'T_STM_PRODUCT'
