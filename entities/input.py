170535a5e7700d6d4dab902398a344d24dedf1b8
hotequil <hotequil@protonmail.com>
2022-10-17 21:47:47 -0300

    feat: changed properties in Input class

--- entities/input.py
+++ entities/input.py
@@ -1,11 +1,12 @@
 class Input:
-    def __init__(self, input_id, product_id, product_quantity, has_product_expiration, is_donation, created_by_user_id, is_active, dt_entered, dt_created, dt_updated, unit_price, input_description):
+    def __init__(self, input_id, product, category, product_quantity, has_product_expiration, is_donation, created_by, is_active, dt_entered, dt_created, dt_updated, unit_price, input_description):
         self.input_id = input_id
-        self.product_id = product_id
+        self.product = product
+        self.category = category
         self.product_quantity = product_quantity
         self.has_product_expiration = has_product_expiration
         self.is_donation = is_donation
-        self.created_by_user_id = created_by_user_id
+        self.created_by = created_by
         self.is_active = is_active
         self.dt_entered = dt_entered
         self.dt_created = dt_created
