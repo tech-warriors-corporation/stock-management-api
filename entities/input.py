f8c6c5849ad2d07febe4faffcc65190724bb65bd
hotequil <hotequil@protonmail.com>
2022-11-09 22:02:50 -0300

    feat: removed is_active from input and output entity

--- entities/input.py
+++ entities/input.py
@@ -1,7 +1,7 @@
 from utils.number import is_number
 
 class Input:
-    def __init__(self, input_id, product, category, product_quantity, has_product_expiration, is_donation, created_by, is_active, dt_entered, dt_created, dt_updated, unit_price, input_description):
+    def __init__(self, input_id, product, category, product_quantity, has_product_expiration, is_donation, created_by, dt_entered, dt_created, dt_updated, unit_price, input_description):
         self.input_id = input_id
         self.product = product
         self.category = category
@@ -9,7 +9,6 @@ class Input:
         self.has_product_expiration = has_product_expiration
         self.is_donation = is_donation
         self.created_by = created_by
-        self.is_active = is_active
         self.dt_entered = dt_entered
         self.dt_created = dt_created
         self.dt_updated = dt_updated
