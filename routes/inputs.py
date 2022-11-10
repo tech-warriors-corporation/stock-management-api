7777dba8688fdbe5c5d718dfd5f02b38377a8993
hotequil <hotequil@protonmail.com>
2022-11-10 20:35:26 -0300

    feat: created get_input and delete_input requests

--- routes/inputs.py
+++ routes/inputs.py
@@ -10,7 +10,7 @@ from entities.input import Input
 from utils.connection import get_connection
 from flask import request
 from utils.date import format_to_iso
-from routes.products import get_product
+from routes.products import get_product, update_product_quantity
 from routes.categories import get_category
 from routes.users import get_user
 
@@ -24,14 +24,14 @@ def inputs():
         page = int(query_params.get('page'))
         per_page = int(query_params.get('per_page'))
         product_id = query_params.get('product_id')
-        from_and_where = f"FROM {Table.INPUTS.value} WHERE IS_ACTIVE = {BooleanAsNumber.TRUE.value}"
+        from_and_where = f"FROM {Table.INPUTS.value} WHERE INPUT_ID IS NOT NULL"
         data = []
 
         if product_id is not None and product_id.isnumeric():
             from_and_where = f"{from_and_where} AND PRODUCT_ID = {int(product_id)}"
 
         cursor.execute(
-            f"SELECT INPUT_ID, PRODUCT_ID, PRODUCT_QUANTITY, HAS_PRODUCT_EXPIRATION, IS_DONATION, CREATED_BY_USER_ID, IS_ACTIVE, DT_ENTERED, DT_CREATED, UNIT_PRICE, INPUT_DESCRIPTION "
+            f"SELECT INPUT_ID, PRODUCT_ID, PRODUCT_QUANTITY, HAS_PRODUCT_EXPIRATION, IS_DONATION, CREATED_BY_USER_ID, DT_ENTERED, DT_CREATED, UNIT_PRICE, INPUT_DESCRIPTION "
             f"{from_and_where} ORDER BY DT_ENTERED DESC, DT_CREATED DESC, INPUT_ID DESC OFFSET {page * per_page} ROWS FETCH NEXT {per_page} ROWS ONLY"
         )
 
@@ -48,13 +48,12 @@ def inputs():
             is_donation = item[4]
             created_by_user_id = item[5]
             created_by = get_user(created_by_user_id, False)[0]['data']
-            is_active = item[6]
-            dt_entered = format_to_iso(item[7])
-            dt_created = format_to_iso(item[8])
-            unit_price = item[9]
-            input_description = item[10]
+            dt_entered = format_to_iso(item[6])
+            dt_created = format_to_iso(item[7])
+            unit_price = item[8]
+            input_description = item[9]
 
-            data.append(Input(input_id, product, category, product_quantity, has_product_expiration, is_donation, created_by, is_active, dt_entered, dt_created, None, unit_price, input_description).__dict__)
+            data.append(Input(input_id, product, category, product_quantity, has_product_expiration, is_donation, created_by, dt_entered, dt_created, None, unit_price, input_description).__dict__)
 
         cursor.execute(f"SELECT COUNT(*) {from_and_where}")
 
@@ -63,5 +62,47 @@ def inputs():
         cursor.close()
 
         return create_response(data, count=count)
+    except:
+        return create_response(None, StatusCode.BAD_REQUEST.value)
+
+@app.route(f'/{api_prefix}/inputs/<int:input_id>', methods=[HttpMethod.GET.value])
+@login_required
+def get_input(input_id):
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        from_and_where = f"FROM {Table.INPUTS.value} WHERE INPUT_ID = {input_id}"
+
+        cursor.execute(f"SELECT INPUT_ID, PRODUCT_ID, PRODUCT_QUANTITY {from_and_where}")
+
+        result = cursor.fetchone()
+        product_id = result[1]
+        product = get_product(product_id, False)[0]['data']
+
+        cursor.close()
+
+        return create_response(Input(input_id, product, None, result[2], None, None, None, None, None, None, None, None))
+    except:
+        return create_response(None, StatusCode.BAD_REQUEST.value)
+
+@app.route(f'/{api_prefix}/inputs/<int:input_id>', methods=[HttpMethod.DELETE.value])
+@login_required
+def delete_input(input_id):
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        input_data = get_input(input_id)[0]['data']
+        product = input_data['product']
+        response = update_product_quantity(product['product_id'], input_data['product_quantity'], False, cursor)
+        status_code = response[1]
+
+        if status_code == StatusCode.BAD_REQUEST.value:
+            return create_response(None, StatusCode.BAD_REQUEST.value)
+
+        cursor.execute(f"DELETE FROM {Table.INPUTS.value} WHERE INPUT_ID = {input_id}")
+        connection.commit()
+        cursor.close()
+
+        return create_response()
     except:
         return create_response(None, StatusCode.BAD_REQUEST.value)
