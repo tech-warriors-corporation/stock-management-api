d04e015faf6db64d5fcbb1f363d6ab046a87a143
hotequil <hotequil@protonmail.com>
2022-11-22 11:43:40 -0300

    feat(entity): added new attributes in output

--- entities/output.py
+++ entities/output.py
@@ -1,11 +1,12 @@
 class Output:
-    def __init__(self, output_id, product_id, product_quantity, has_product_expiration, product_went_to, created_by_user_id, dt_exited, dt_created, dt_updated, output_description):
+    def __init__(self, output_id, product, category, product_quantity, has_product_expiration, product_went_to, created_by, dt_exited, dt_created, dt_updated, output_description):
         self.output_id = output_id
-        self.product_id = product_id
+        self.product = product
+        self.category = category
         self.product_quantity = product_quantity
         self.has_product_expiration = has_product_expiration
         self.product_went_to = product_went_to
-        self.created_by_user_id = created_by_user_id
+        self.created_by = created_by
         self.dt_exited = dt_exited
         self.dt_created = dt_created
         self.dt_updated = dt_updated
